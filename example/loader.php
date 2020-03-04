3d94e591ac61894fa37a7a02f09e8f84d20387d1
Ali Hasan <alihasan.iubat@gmail.com>
2020-03-04 08:02:12 +0600

    git init

---
+++ example/loader.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once(__DIR__ .'/../vendor/autoload.php');
+
+spl_autoload_register(function ($class){
+	$class = str_replace('Kodeio\TwitterScraper\\', '', $class);
+	$file = __DIR__ .'/../src/'. $class .'.php';
+	if(file_exists($file)){
+		require_once($file);
+	}
+});
